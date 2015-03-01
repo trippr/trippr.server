3f5e0ec6c6a8d8a766ee2e226a8f7c2f3f789794
Ricardo Vegas <ricardo.vegas@cakedc.com>
2015-03-01 18:36:41 +0100

    Adding test file for PHP search

--- api.php
+++ api.php
@@ -7,6 +7,38 @@ $app->get('/hello/:name', function ($name) {
 });
 $app->get('/destinations/search/:text(/:excluded)', function($text, $excluded="") {
     echo "text is: ". $text;
+	echo "<br>";
     echo "excluded is: ". $excluded;
+
+	$data = array(
+		"query" => array(
+            "more_like_this" => array(
+                "like_text" => $text,
+                "min_term_freq" => 1,
+                "min_doc_freq" => 1,
+                "max_doc_freq" => 10
+			)
+		)
+	);
+
+	$datajson = json_encode($data);
+
+	// Get cURL resource
+	$curl = curl_init();
+	// Set some options - we are passing in a useragent too here
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'http://54.191.28.250:9200/trippr/_search',
+		CURLOPT_USERAGENT => 'Internal API Server',
+		CURLOPT_POST => 1,
+		CURLOPT_POSTFIELDS => $datajson
+	));
+	// Send the request & save response to $resp
+	$resp = curl_exec($curl);
+	// Close request to clear up some resources
+	curl_close($curl);
+
+	echo "<br>";
+	var_dump($resp);
 });
 $app->run();
