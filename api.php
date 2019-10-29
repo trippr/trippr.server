faf14dd53f634cb2f1b593449e88ae9d9a614f24
rvegas <rvegas@users.noreply.github.com>
2019-10-29 20:48:04 +0100

    Fix code formatting

--- api.php
+++ api.php
@@ -2,64 +2,51 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-        echo "Hello, $name";
-});
 $app->get('/destinations/search/:text(/:excluded)', function($text, $excluded="") {
-    //echo "text is: ". $text;
-	//echo "<br>";
     $excludedList = explode(',', $excluded);
-
-	$data = array(
-		"query" => array(
-            "more_like_this" => array(
-                "like_text" => $text,
-                "min_term_freq" => 1,
-                "min_doc_freq" => 1,
-                "max_doc_freq" => 10
-			)
-		)
-	);
-
+    $data = array(
+        "query" => array(
+        "more_like_this" => array(
+        "like_text" => $text,
+        "min_term_freq" => 1,
+        "min_doc_freq" => 1,
+        "max_doc_freq" => 10
+        )
+     ));
+        
 	$datajson = json_encode($data);
 
-	// Get cURL resource
-	$curl = curl_init();
-	// Set some options - we are passing in a useragent too here
-	curl_setopt_array($curl, array(
-		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_URL => 'http://54.191.28.250:9200/trippr/_search',
-		CURLOPT_USERAGENT => 'Internal API Server',
-		CURLOPT_POST => 1,
-		CURLOPT_POSTFIELDS => $datajson
-	));
-	// Send the request & save response to $resp
-	$resp = curl_exec($curl);
-	// Close request to clear up some resources
-	curl_close($curl);
-	//var_dump($resp);
+        // Get cURL resource
+        $curl = curl_init();
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => 'http://54.191.28.250:9200/trippr/_search',
+                CURLOPT_USERAGENT => 'Internal API Server',
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => $datajson
+        ));
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        // Close request to clear up some resources
+        curl_close($curl);
 
-	$responsejson = json_decode($resp, true);
+        $responsejson = json_decode($resp, true);
 
-	//var_dump($responsejson['hits']);
+        $hits = isset($responsejson['hits']['hits']) ? $responsejson['hits']['hits'] : array();
 
-	$hits = isset($responsejson['hits']['hits']) ? $responsejson['hits']['hits'] : array();
+        $city = "";
 
-	$city = "";
+        foreach($hits as $hit) {
+                $name = $hit['_source']['name'];
+                $country = $hit['_source']['country'];
+                $countrycode = $hit['_source']['countrycode'];
+                if(!in_array($name, $excludedList)) {
+                        $city = $name . " + " . $country . " + " . $countrycode;
+                        return $city;
+                }
+        }
 
-	foreach($hits as $hit) {
-		$name = $hit['_source']['name'];
-		$country = $hit['_source']['country'];
-		$countrycode = $hit['_source']['countrycode'];
-
-		if(!in_array($name, $excludedList)) {
-			$city = $name . " + " . $country . " + " . $countrycode;
-			echo $city;
-			return $city;
-		}
-	}
-
-	echo "notfound";
-	return "notfound";
+        return "notfound";
 });
 $app->run();
